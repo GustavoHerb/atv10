728812a25d0ba2ae7b572c155417cda82e513136
GustavoHerb <141774477+GustavoHerb@users.noreply.github.com>
2024-03-31 11:04:16 -0300

    Add files via upload

---
+++ script2.js
@@ -0,0 +1,10 @@
+        // Perguntar a idade ao usuário
+        var idade = Number(prompt("Por favor, digite sua idade:"));
+
+        if (idade < 18) {
+            alert("Você é menor de idade.");
+        } else if (idade === 18) {
+            alert("Você acabou de atingir a maioridade.");
+  }     else {
+            alert("Você é maior de idade.");
+     }
